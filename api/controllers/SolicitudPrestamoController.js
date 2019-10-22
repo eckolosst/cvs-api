47b92acee65222dd8a116267103dde9bcabe1f22
agus <agusgc_99@hotmail.com>
2019-10-22 13:50:55 -0300

    Algunos controllers

--- api/controllers/SolicitudPrestamoController.js
+++ api/controllers/SolicitudPrestamoController.js
@@ -6,7 +6,43 @@
  */
 
 module.exports = {
-  
+    aprobarSolicitud: (req, res) => {
+        var solicitudId = req.param('_id');
+
+        if (!_.isNumeric(solicitudId)) {
+            return res.badRequest(new Error('Error en el id de la solicitud'));
+          }
+
+        let SolicitudPrestamoItem = SolicitudPrestamo.findOne({id:solicitudId});
+
+        if(!SolicitudPrestamoItem){
+            return res.badRequest(new Error('Error al encontrar la solicitud'));
+        }
+
+        SolicitudPrestamoItem.updateOne({resultado: 'Aprobado'});
+        SolicitudPrestamoItem.updateOne({fechaAprobacionRechazo: new Date()})
+
+        return res.send("succes")
+      }
+  ,
+      rechazarSolicitud: (req, res) => {
+        var solicitudId = req.param('_id');
+
+        if (!_.isNumeric(solicitudId)) {
+            return res.badRequest(new Error('Error en el id de la solicitud'));
+          }
+
+        let SolicitudPrestamoItem = SolicitudPrestamo.findOne({id:solicitudId});
+
+        if(!SolicitudPrestamoItem){
+            return res.badRequest(new Error('Error al encontrar la solicitud'));
+        }
+
+        SolicitudPrestamoItem.updateOne({resultado: 'Rechazada'});
+        SolicitudPrestamoItem.updateOne({fechaAprobacionRechazo: new Date()})
+
+        return res.send("succes")
+      }
 
 };
 
