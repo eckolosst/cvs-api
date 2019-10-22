470bb94ce614a660406f34c5b3cc4baf6ae3ebab
ecko <lucasp.1606@gmail.com>
2019-10-22 08:26:53 -0300

    agrega SolicitudPrestamo controller

---
+++ api/controllers/SolicitudPrestamoController.js
@@ -0,0 +1,12 @@
+/**
+ * SolicitudPrestamoController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+  
+
+};
+
